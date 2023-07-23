9b040c69e4b50a1a4b7981fa5dfadc7948832279
Humna <humnasultan@gmail.com>
2023-07-23 16:25:43 -0400

    added ternaryOp basics proj

---
+++ summer 2023/basicsPractice/ternaryOp/ternaryOp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//ternary ? - replacement of if/else
+	// condition ? expression 1 : expression 2
+
+	int nom = 10;
+
+	nom >= 15 ? cout << "yay!" : cout << "not big enough";
+
+	return 0;
+}
