ed70c81c6eb9533bb4b7cee78b608f2f31840218
Humna <humnasultan@gmail.com>
2023-07-25 23:34:04 -0400

    Update tictactoe.h

--- summer 2023/projects/tictactoe/tictactoe.h
+++ summer 2023/projects/tictactoe/tictactoe.h
@@ -1,6 +1,8 @@
 #pragma once
 #ifndef TICTACTOE.H
 #define TICTACTOE.H
+//known as "header guards" - prevent redeclaration of any identifiers such as types, enums, classes, and static variables
+//used to prevent code from being included multiple times
 
 class game_class
 {
