20d82c8f237787124ca82504927bdf28bad2476e
Humna <humnasultan@gmail.com>
2023-07-31 19:59:49 -0400

    Update overloadEx.cpp

--- summer 2023/basicsPractice/overloadFunc/overloadEx.cpp
+++ summer 2023/basicsPractice/overloadFunc/overloadEx.cpp
@@ -4,6 +4,18 @@ int main() {
 	return 0;
 }
 
+//overloading - sharing names, diff parameters
+//function name + parameters = signature
+//each signature needs to be unique - like an ID!!
+
 void bakePizza() {
 	std::cout << "Here is your pizza!\n";
+}
+
+void bakePizza(std::string topping1) {
+	std::cout << "Here is your " << topping1 << " pizza!\n";
+}
+
+void bakePizza(std::string topping1, std::string topping2) {
+	std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza!\n";
 }
