3bd0400047928cf394874cc558a2df921a669a71
Humna <humnasultan@gmail.com>
2023-07-31 19:33:42 -0400

    Update functions.cpp

---
+++ summer 2023/basicsPractice/stringFuncs/functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	std::string name;
+
+	std::cout << "Enter your name:: " << endl;
+	std::getline(std::cin, name);
+
+
+	return 0;
+}
