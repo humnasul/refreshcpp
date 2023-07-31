e22b89abc6a7ec1303bcb06a16db8eb5361f1123
Humna <humnasultan@gmail.com>
2023-07-31 19:36:18 -0400

    Update functions.cpp

--- summer 2023/basicsPractice/stringFuncs/functions.cpp
+++ summer 2023/basicsPractice/stringFuncs/functions.cpp
@@ -8,6 +8,20 @@ int main() {
 	std::cout << "Enter your name:: " << endl;
 	std::getline(std::cin, name);
 
+	//using length()
+	if (name.length() > 12) {
+		std::cout << "Your name can't be over 12 characters.";
+	}
+	else {
+		std::cout << "Welcome " << name;
+	}
+
+	name.empty();
+	//returns boolean value
+
+	if (name.empty()) {
+		std::cout << "You didn't enter your name." << endl;
+	}
 
 	return 0;
 }
