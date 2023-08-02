768b6eda1b5e8b8b6d35a8226301e7db2266f13c
Humna <humnasultan@gmail.com>
2023-08-01 22:18:01 -0400

    Update scope.cpp

--- summer 2023/basicsPractice/varScope/scope.cpp
+++ summer 2023/basicsPractice/varScope/scope.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+int myNum = 3;
+//global variable :)
+
 int main() {
 	int myNum = 1;
+	std::cout << myNum;
+	//prints local variable value (1)
 
 	return 0;
 }
@@ -9,3 +14,14 @@ int main() {
 void printNum() {
 	//cannot access 'myNum' variable in main!!
 }
+
+void printNum2(int myNum) {
+	std::cout << myNum;
+	//has a diff scope from the 'myNum' in main - this works, but its 2 different vars!!
+}
+
+void printNum3() {
+	std::cout << myNum;
+}
+//try to avoid global variables because it fills up global namespace
+//variables stored within a function are more secure!!
