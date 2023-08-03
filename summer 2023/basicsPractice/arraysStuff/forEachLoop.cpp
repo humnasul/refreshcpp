516408651005401ca1ffe35206bd92175512122d
Humna <humnasultan@gmail.com>
2023-08-02 23:00:22 -0400

    added for each stuff

---
+++ summer 2023/basicsPractice/arraysStuff/forEachLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//foreach loop = loop that eases the traversal over an iterable dataset
+
+	std::string students[] = { "Spongebob", "Patrick", "Squidward" };
+
+	for (int i = 0; i < sizeof(students) / sizeof(students[0]); i++) {
+		std::cout << students[i] << std:: endl;
+	}
+	//the for loop has sizeof(students) / sizeof(students[0]) to get the num of elements!
+
+	return 0;
+}
