0454e8045a8a724ed5e3732b469cfce09be42c02
Humna <humnasultan@gmail.com>
2023-08-02 23:11:10 -0400

    Update forEachLoop.cpp

--- summer 2023/basicsPractice/arraysStuff/forEachLoop.cpp
+++ summer 2023/basicsPractice/arraysStuff/forEachLoop.cpp
@@ -5,11 +5,18 @@ int main() {
 	//foreach loop = loop that eases the traversal over an iterable dataset
 
 	std::string students[] = { "Spongebob", "Patrick", "Squidward" };
+	//you can also increase this size
+	int grades[] = { 65, 71, 90, 56 };
 
 	for (int i = 0; i < sizeof(students) / sizeof(students[0]); i++) {
 		std::cout << students[i] << std:: endl;
 	}
 	//the for loop has sizeof(students) / sizeof(students[0]) to get the num of elements!
+
+	for (std::string student : students) {
+		std::cout << student;
+	}
+	//for each loop :)
 
 	return 0;
 }
