605eb42988c7524d89b13acdecda6b9364f79314
Humna <humnasultan@gmail.com>
2023-08-02 22:47:52 -0400

    Update sizeOf.cpp

--- summer 2023/basicsPractice/arraysStuff/sizeOf.cpp
+++ summer 2023/basicsPractice/arraysStuff/sizeOf.cpp
@@ -2,8 +2,12 @@
 
 int main() {
 
-	//sizeOf() = determines the size in bytes of a variable, dataType, class, object, etc.
+	//sizeof() = determines the size in bytes of a variable, dataType, class, object, etc.
 
+	double gpa = 2.5;
 
+	std::cout << sizeof(gpa) << " bytes\n";
+
+	return 0;
 
 }
