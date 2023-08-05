b5c6baaf7304c75d697d1339204be609641abb06
Humna <humnasultan@gmail.com>
2023-08-04 22:04:56 -0400

    Update searchArray.cpp

--- summer 2023/basicsPractice/arraysStuff/searchArray.cpp
+++ summer 2023/basicsPractice/arraysStuff/searchArray.cpp
@@ -2,14 +2,20 @@
 
 int main() {
 	int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int size = sizeof(numbers) / sizeof(numbers[0]);
+	std::string foods[] = { "candy", "pizza", "chocolate", "mac and cheese" };
+
+	int size1 = sizeof(numbers) / sizeof(numbers[0]);
+	int size2 = sizeof(foods) / sizeof(foods[0]);
 
 	int index, myNum;
+	//std::string myFood;
 
 	std::cout << "Enter element to search for :: " << "\n";
 	std::cin >> myNum;
+	//std::getline(std::cin, myFood);
 
-	index = searchArray(numbers, size, myNum);
+	index = searchArray(numbers, size1, myNum);
+	//call could also be using food
 
 	if (index != -1) {
 		std::cout << myNum << " is at " << index;
@@ -22,8 +28,11 @@ int main() {
 }
 
 int searchArray(int array[], int size, int element) {
+	//change to strings for myFoods
+
 	for (int i = 0; i < size; i++) {
 		if (array[i] == element) {
+			//you can uxe == for strings in c++!
 			return i;
 		}
 	}
