6fc66fb06f147d74a6e9456092ed13c668961de9
Humna <humnasultan@gmail.com>
2023-08-04 21:57:40 -0400

    added search algorithm

---
+++ summer 2023/basicsPractice/arraysStuff/searchArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main() {
+	int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	int size = sizeof(numbers) / sizeof(numbers[0]);
+
+	int index, myNum;
+
+	std::cout << "Enter element to search for :: " << "\n";
+	std::cin >> myNum;
+}
