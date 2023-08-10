aabf9371c0901e1f507c1bbf5ccc9b40dd234c20
Humna <humnasultan@gmail.com>
2023-08-09 23:21:10 -0400

    Create nullPoint.cpp

---
+++ summer 2023/basicsPractice/pointers/nullPoint.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	//Null value - a special value that means something has no value
+	// when a pointer is holding a null value, that pointer is not pointing at anything (null pointer)
+
+	//nullptr = keyword that represents a null pointer literal
+
+	//nullptrs - helpful when determining if an address was successfully assigned to a pointer
+}
