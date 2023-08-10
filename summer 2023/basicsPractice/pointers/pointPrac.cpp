64e170abb7a2eb64384c5a35031f9c776ded08e5
Humna <humnasultan@gmail.com>
2023-08-09 23:18:09 -0400

    finished pointer basics

--- summer 2023/basicsPractice/pointers/pointPrac.cpp
+++ summer 2023/basicsPractice/pointers/pointPrac.cpp
@@ -8,6 +8,7 @@ int main() {
 
 	std::string name = "Humna";
 	int age = 21;
+	std::string freeCandy[5] = { "reeses1", "reeses2", "reeses3", "reeses4", "reeses5" };
 
 	std::string* pName = &name;
 	//how to make a pointer
@@ -15,6 +16,12 @@ int main() {
 	// use deference operator (*) to access value in pointer!
 
 	int* pAge = &age;
+	
+	//std::string *pFreeCandy = &freeCandy;
+	//this gives an error because array is already an address and does not need &!
+	std::string* pFreeCandy = freeCandy;
+	std::cout << *pFreeCandy << '\n';
+	//gives first element of freeCandy!!
 
 	return 0;
 }
