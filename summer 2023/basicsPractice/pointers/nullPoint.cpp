ab0c89ee168460fb867fcc672d7cd64c3b27de6a
Humna <humnasultan@gmail.com>
2023-08-09 23:25:27 -0400

    added null pointer content :)

--- summer 2023/basicsPractice/pointers/nullPoint.cpp
+++ summer 2023/basicsPractice/pointers/nullPoint.cpp
@@ -7,4 +7,22 @@ int main() {
 	//nullptr = keyword that represents a null pointer literal
 
 	//nullptrs - helpful when determining if an address was successfully assigned to a pointer
+
+	int* pointer = nullptr;
+	// if you do not want to assign a value right away, this is standard
+	// do not dereference a pointer pointing at null!
+	int x = 123;
+
+	pointer = &x;
+
+	if (pointer == nullptr) {
+		std::cout << "address was not assigned!\n";
+		//std::cout << *pointer; - NO
+	}
+	else {
+		std::cout << "address was successfully assigned\n";
+		std::cout << *pointer;
+	}
+
+	return 0;
 }
