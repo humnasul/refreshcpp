b47538aa9de0806526dd6fb95c40e395d87f5628
Humna <humnasultan@gmail.com>
2023-08-10 23:51:14 -0400

    Update recursion.cpp

--- summer 2023/basicsPractice/functionStuff/recursion.cpp
+++ summer 2023/basicsPractice/functionStuff/recursion.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 void walk(int steps);
+int factorial1(int num);
+int factorial2(int num);
 
 int main() {
 	//recursion - a programming technique where a function invokes itself from within
@@ -9,7 +11,6 @@ int main() {
 	//				recursion can use more memory and be slower :( depends on case
 
 	walk(100);
-
 
 
 	return 0;
@@ -24,5 +25,17 @@ void walk(int steps) {
 		std::cout << "You take a step\n";
 		walk(steps - 1);
 		//calling function within itself
+		//need the '-1' or there is a stack overflow
 	}
+}
+
+int factorial1(int num) {
+	int result = 1;
+	for (int i = 1; i <= num; i++) {
+		result = result * i;
+	}
+}
+
+int factorial2(int num) {
+	
 }
