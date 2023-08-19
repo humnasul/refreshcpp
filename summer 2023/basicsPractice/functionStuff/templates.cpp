117f4a8fbabbda76f6c2c454cab918227f2629f8
Humna <humnasultan@gmail.com>
2023-08-18 23:24:49 -0400

    updates

--- summer 2023/basicsPractice/functionStuff/templates.cpp
+++ summer 2023/basicsPractice/functionStuff/templates.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-template <typename T>
+template <typename T, typename U>
+// adding 'typename U' allows you to have up to 2 vars of diff types!
 
-T max(T x, T y) {
+T max(T x, U y) {
 	//use a generic 'T' instead of 'int' or 'double'
+	//you can use 'T' or 'U' as types for x and y!
 	return (x > y) ? x : y;
 }
 
