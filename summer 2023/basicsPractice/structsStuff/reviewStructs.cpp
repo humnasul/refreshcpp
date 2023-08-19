20a359d915472cb71ad5a3ea5ee03621d00733e6
Humna <humnasultan@gmail.com>
2023-08-18 23:39:40 -0400

    Update reviewStructs.cpp

--- summer 2023/basicsPractice/structsStuff/reviewStructs.cpp
+++ summer 2023/basicsPractice/structsStuff/reviewStructs.cpp
@@ -14,15 +14,28 @@ int main() {
 	//	members can access with . aka "'ass member access operator"
 
 	student student1;
-	// struct identifer name
+	// format: struct identifer name
 	student1.name = "Humna";
 	student1.gpa = 3.8;
 	student1.enrolled = true;
 	//member can be accessed with . !
 
+	student student2;
+	// struct identifer name
+	student2.name = "Coco";
+	student2.gpa = 4.0;
+	student2.enrolled = true;
+	//member can be accessed with . !
+
 	std::cout << student1.name << '\n';
 	std::cout << student1.gpa << '\n';
 	std::cout << student1.enrolled << '\n';
+
+	std::cout << student2.name << '\n';
+	std::cout << student2.gpa << '\n';
+	std::cout << student2.enrolled << '\n';
+
+	//each struct made can have diff values!!
 
 	return 0;
 }
