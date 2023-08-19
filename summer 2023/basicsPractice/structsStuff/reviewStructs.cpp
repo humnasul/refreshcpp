42afd900f3cfd4115de3a48e1de68f2290e5ba11
Humna <humnasultan@gmail.com>
2023-08-18 23:29:39 -0400

    added structs review items

---
+++ summer 2023/basicsPractice/structsStuff/reviewStructs.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	// struct - groups related variables under one name
+	//	structs contain many different data types
+	//	variables in a struct are known as 'members'
+	//	members can access with . aka "'ass member access operator"
+
+	return 0;
+}
