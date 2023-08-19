85281002e7e6cd26072f7bf38dfc70646696e115
Humna <humnasultan@gmail.com>
2023-08-18 23:21:26 -0400

    Update templates.cpp

--- summer 2023/basicsPractice/functionStuff/templates.cpp
+++ summer 2023/basicsPractice/functionStuff/templates.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+int max(int x, int y) {
+	return (x > y) ? x : y;
+}
+
 int main() {
 	// function template - describes what a function looks like
 	//	can be used to generate as many overloaded functions as needed,
 	//	each using different data types
+
+	std::cout << max(1, 2) << '\n';
+	//you can't use this with doubles, which is inconvenient! - you can make an overloaded function, but that's annoying
+
+	return 0;
 }
