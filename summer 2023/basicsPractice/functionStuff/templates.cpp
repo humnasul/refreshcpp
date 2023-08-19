fda8430fe3c79ea5d917db37a05975befa5a0b9d
Humna <humnasultan@gmail.com>
2023-08-18 23:19:24 -0400

    Create templates.cpp

---
+++ summer 2023/basicsPractice/functionStuff/templates.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	// function template - describes what a function looks like
+	//	can be used to generate as many overloaded functions as needed,
+	//	each using different data types
+}
