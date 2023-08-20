3dd6f877cc2554303551013b3121add1f83931c8
Humna <humnasultan@gmail.com>
2023-08-19 22:59:00 -0400

    basic info

--- summer 2023/basicsPractice/structsStuff/passingStructs.cpp
+++ summer 2023/basicsPractice/structsStuff/passingStructs.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+struct Car {
+	std::string model;
+	int year;
+	std::string color;
+};
+
 int main() {
+
+	Car car1;
+	Car car2;
+	// two structs!!
+
+	car1.model = "model1";
+	car1.year = 2000;
+	car1.color = "pink";
+
+	car2.model = "model2";
+	car2.year = 2004;
+	car2.color = "purple";
+
 
 	return 0;
 }
